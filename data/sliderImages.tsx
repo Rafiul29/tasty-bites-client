882024e190c87b088a21755165c2db540dc39b82
Rafiul29 <rafiulislam4472@gmail.com>
2023-05-31 00:49:23 +0600

    navbar and slider added

---
+++ data/sliderImages.tsx
@@ -0,0 +1,35 @@
+
+export const data = [
+  {
+    id: 1,
+    src: "https://images.pexels.com/photos/12325276/pexels-photo-12325276.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Burger and Fries on a Wooden Board",
+    headline: "Delicious Burger",
+    paragraph: "Enjoy our mouthwatering burger made with fresh ingredients.",
+    cta: "Order Now"
+  },
+  {
+    id: 2,
+    src: "https://images.pexels.com/photos/15813490/pexels-photo-15813490/free-photo-of-delicious-pizza-with-sauces-on-table.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Delicious Pizza with Sauces on Table",
+    headline: "Authentic Pizza",
+    paragraph: "Indulge in our delicious and cheesy pizza made from the finest ingredients.",
+    cta: "Order Now"
+  },
+  {
+    id: 3,
+    src: "https://images.pexels.com/photos/15832874/pexels-photo-15832874/free-photo-of-ready-to-eat-pasta-with-basil-and-cherry-tomatoes.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Ready-to-Eat Pasta with Basil and Cherry Tomatoes",
+    headline: "Creamy Pasta Delight",
+    paragraph: "Savor our creamy pasta dish prepared with a blend of flavors.",
+    cta: "Order Now"
+  },
+  {
+    id: 4,
+    src: "https://images.pexels.com/photos/725991/pexels-photo-725991.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Cooked Meat With Vegetables",
+    headline: "Fresh Grilled Fish",
+    paragraph: "Experience the taste of the ocean with our perfectly grilled fish.",
+    cta: "Order Now"
+  }
+];
